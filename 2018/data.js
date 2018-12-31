58cd87a9f77937a1c17fa55d84044fa2c81aa876
Hossain <shossain@SHOSSAIN-MBP.local>
2018-12-31 13:23:38 -0600

    Adding boilerplate for 2018

---
+++ 2018/data.js
@@ -0,0 +1,68 @@
+var _DATA = {
+  year: 2017,
+  title: 'TwentySeventeen',
+  backgroundColor: '#292a25',
+  spotify: 'https://open.spotify.com/user/suraiya773/playlist/66k63NE8Np6mzhY3ZSDMUm',
+  tracks: [
+    { src: 'tracks/01-TheXX-OnHold.mp3',
+      title: 'On Hold',
+      artist: 'The xx'
+    },
+    { src: 'tracks/02-JensLekman-HotwireTheFerrisWheel.mp3',
+      title: 'Hotwire the Ferris Wheel',
+      artist: 'Jens Lekman'
+    },
+    { src: 'tracks/03-SylvanEsso-Song.mp3',
+      title: 'Song',
+      artist: 'Sylvan Esso'
+    },
+    { src: 'tracks/04-LuisFonsiDaddyYankee-Despacito.mp3',
+      title: 'Despacito',
+      artist: 'Luis Fonsi & Daddy Yankee'
+    },
+    { src: 'tracks/05-ShahidMallya-Radha.mp3',
+      title: 'Radha',
+      artist: 'Shahid Mallya & Sunidhi Chauhan'
+    },
+    { src: 'tracks/06-HamiltonMixTape-Immigrants.mp3',
+      title: 'Immigrants (We Get The Job Done)',
+      artist: 'Hamilton Mix Tape'
+    },
+    { src: 'tracks/07-JamilaWoods-Holy.mp3',
+      title: 'Holy',
+      artist: 'Jamila Woods'
+    },
+    { src: 'tracks/08-HAIM-WantYouBack.mp3',
+      title: 'Want You Back',
+      artist: 'HAIM'
+    },
+    { src: 'tracks/09-MileyCyrus-Malibu.mp3',
+      title: 'Malibu',
+      artist: 'Miley Cyrus'
+    },
+    { src: 'tracks/10-ILOVEMAKONNEN-Love.mp3',
+      title: 'Love',
+      artist: 'ILOVEMAKONNEN'
+    },
+    { src: 'tracks/11-Rostam-BikeDream.mp3',
+      title: 'Bike Dream',
+      artist: 'Rostam'
+    },
+    { src: 'tracks/12-StVincent-NewYork.mp3',
+      title: 'New York',
+      artist: 'St. Vincent'
+    },
+    { src: 'tracks/13-PerfumeGenius-SlipAway.mp3',
+      title: 'Slip Away',
+      artist: 'Perfume Genius'
+    },
+    { src: 'tracks/14-Lorde-HomemadeDynamite.mp3',
+      title: 'Homemade Dynamite',
+      artist: 'Lorde'
+    },
+    { src: 'tracks/15-JayZ-Bam.mp3',
+      title: 'Bam (feat. Damian Marley)',
+      artist: 'JAY-Z'
+    }
+  ]
+};
