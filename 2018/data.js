bb785df7620ef798e39c7d6d0f69e9842d898602
Monsur Hossain <monsur@gmail.com>
2018-12-31 20:27:10 -0600

    Updating track listing

--- 2018/data.js
+++ 2018/data.js
@@ -4,65 +4,65 @@ var _DATA = {
   backgroundColor: '#292a25',
   spotify: 'https://open.spotify.com/user/suraiya773/playlist/2s6Y3wLWVzpSv5vP9RyTHt',
   tracks: [
-    { src: 'tracks/01-TheXX-OnHold.mp3',
-      title: 'On Hold',
-      artist: 'The xx'
+    { src: 'tracks/01-TroyeSivan-MyMyMy.mp3',
+      title: 'My My My!',
+      artist: 'Troye Sivan'
     },
-    { src: 'tracks/02-JensLekman-HotwireTheFerrisWheel.mp3',
-      title: 'Hotwire the Ferris Wheel',
-      artist: 'Jens Lekman'
+    { src: 'tracks/02-MattAndKim-Forever.mp3',
+      title: 'Forever',
+      artist: 'Matt and Kim'
     },
-    { src: 'tracks/03-SylvanEsso-Song.mp3',
-      title: 'Song',
-      artist: 'Sylvan Esso'
+    { src: 'tracks/03-JeffRosenstock-910.mp3',
+      title: '9/10',
+      artist: 'Jeff Rosenstock'
     },
-    { src: 'tracks/04-LuisFonsiDaddyYankee-Despacito.mp3',
-      title: 'Despacito',
-      artist: 'Luis Fonsi & Daddy Yankee'
+    { src: 'tracks/04-LinManuelMiranda-FoundTonight.mp3',
+      title: 'Found/Tonight',
+      artist: 'Lin-Manuel Miranda + Ben Platt'
     },
-    { src: 'tracks/05-ShahidMallya-Radha.mp3',
-      title: 'Radha',
-      artist: 'Shahid Mallya & Sunidhi Chauhan'
+    { src: 'tracks/05-KendrickLamar-AllTheStars.mp3',
+      title: 'All The Stars',
+      artist: 'Kendrick Lamar + SZA'
     },
-    { src: 'tracks/06-HamiltonMixTape-Immigrants.mp3',
-      title: 'Immigrants (We Get The Job Done)',
-      artist: 'Hamilton Mix Tape'
+    { src: 'tracks/06-Saansein-PrateekKuhad.mp3',
+      title: 'Saansein',
+      artist: 'Prateek Kuhad'
     },
-    { src: 'tracks/07-JamilaWoods-Holy.mp3',
-      title: 'Holy',
-      artist: 'Jamila Woods'
+    { src: 'tracks/07-Holliday-TheWeekendRunClub.mp3',
+      title: 'Holliday',
+      artist: 'The Weekend Run Club'
     },
-    { src: 'tracks/08-HAIM-WantYouBack.mp3',
-      title: 'Want You Back',
-      artist: 'HAIM'
+    { src: 'tracks/08-EmpressOf-WhenImWithHim.mp3',
+      title: 'When I\'m With Him',
+      artist: 'Empress Of'
     },
-    { src: 'tracks/09-MileyCyrus-Malibu.mp3',
-      title: 'Malibu',
-      artist: 'Miley Cyrus'
+    { src: 'tracks/09-Yuno-NoGoingBack.mp3',
+      title: 'No Going Back',
+      artist: 'Yuno'
     },
-    { src: 'tracks/10-ILOVEMAKONNEN-Love.mp3',
-      title: 'Love',
-      artist: 'ILOVEMAKONNEN'
+    { src: 'tracks/10-joan-TakeMeOn.mp3',
+      title: 'Take Me On',
+      artist: 'joan'
     },
-    { src: 'tracks/11-Rostam-BikeDream.mp3',
-      title: 'Bike Dream',
-      artist: 'Rostam'
+    { src: 'tracks/11-AndersonPaak-Tints.mp3',
+      title: 'Tints',
+      artist: 'Anderson .Paak + Kendrick Lamar'
     },
-    { src: 'tracks/12-StVincent-NewYork.mp3',
-      title: 'New York',
-      artist: 'St. Vincent'
+    { src: 'tracks/12-Mitski-Nobody.mp3',
+      title: 'Nobody',
+      artist: 'Mitski'
     },
-    { src: 'tracks/13-PerfumeGenius-SlipAway.mp3',
-      title: 'Slip Away',
-      artist: 'Perfume Genius'
+    { src: 'tracks/13-Saba-Smile.mp3',
+      title: 'Smile',
+      artist: 'Saba'
     },
-    { src: 'tracks/14-Lorde-HomemadeDynamite.mp3',
-      title: 'Homemade Dynamite',
-      artist: 'Lorde'
+    { src: 'tracks/14-AmaraLaNegra-WhatABamBam.mp3',
+      title: 'What a Bam Bam',
+      artist: 'Amara La Negra'
     },
-    { src: 'tracks/15-JayZ-Bam.mp3',
-      title: 'Bam (feat. Damian Marley)',
-      artist: 'JAY-Z'
+    { src: 'tracks/15-TheCarters-Friends.mp3',
+      title: 'Friends',
+      artist: 'The Carters'
     }
   ]
 };
