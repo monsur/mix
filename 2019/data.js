4b4385304094d454e6436ec09b758899aceb5e66
Monsur Hossain <monsur@monsurs-mbp.lan>
2020-01-28 22:54:28 -0600

    Adding 2019

---
+++ 2019/data.js
@@ -0,0 +1,84 @@
+var _DATA = {
+  year: 2018,
+  title: 'TwentyNineteen',
+  backgroundColor: '#000000',
+  spotify: 'https://open.spotify.com/user/suraiya773/playlist/2s6Y3wLWVzpSv5vP9RyTHt',
+  tracks: [
+    { src: 'tracks/01-Lizzo-TruthHurts.mp3',
+      title: 'Lizzo',
+      artist: 'Truth Hurts'
+    },
+    { src: 'tracks/02-ShawnMendes-LostInJapan.mp3',
+      title: 'Shawn Mendes',
+      artist: 'Lost in Japan'
+    },
+    { src: 'tracks/03-KatyPerry-NeverReallyOver.mp3',
+      title: 'Katy Perry',
+      artist: 'Never Really Over'
+    },
+    { src: 'tracks/04-JoyCrookes-LondonMine.mp3',
+      title: 'Joy Crookes',
+      artist: 'London Mine'
+    },
+    { src: 'tracks/05-MaggieRogers-Alaska.mp3',
+      title: 'Maggie Rogers',
+      artist: 'Alaska'
+    },
+    { src: 'tracks/06-Clairo-Bags.mp3',
+      title: 'Clairo',
+      artist: 'Bags'
+    },
+    { src: 'tracks/07-LanaDelRey-TheGreatest.mp3.mp3',
+      title: 'Lana Del Rey',
+      artist: 'The greatest'
+    },
+    { src: 'tracks/08-CarlyRaeJespen-TooMuch.mp3',
+      title: 'Carly Rae Jespen',
+      artist: 'Too Much'
+    },
+    { src: 'tracks/09-EdSheeran-IDontCare.mp3',
+      title: 'Ed Sheeran',
+      artist: 'I Don\'t Care'
+    },
+    { src: 'tracks/10-HAIM-SummerGirl.mp3',
+      title: 'HAIM',
+      artist: 'Summer Girl'
+    },
+    { src: 'tracks/11-VampireWeekend-ThisLife.mp3',
+      title: 'Vampire Weekend',
+      artist: 'This Life'
+    },
+    { src: 'tracks/12-Wilco-EveryoneHides.mp3',
+      title: 'Wilco',
+      artist: 'Everyone Hides'
+    },
+    { src: 'tracks/13-SharonVanEtten-Seventeen.mp3',
+      title: 'Sharon Van Etten',
+      artist: 'Seventeen'
+    },
+    { src: 'tracks/14-SleaterKinney-HurryOnHome.mp3',
+      title: 'Sleater-Kinney',
+      artist: 'Hurry On Home'
+    },
+    { src: 'tracks/15-PostMalone-Goodbyes.mp3',
+      title: 'Post Malone',
+      artist: 'Goodbyes (feat. Young Thug)'
+    },
+    { src: 'tracks/16-Khalid-Talk.mp3',
+      title: 'Khalid (feat. Disclosure)',
+      artist: 'Talk'
+    },
+    { src: 'tracks/17-JonasBrothers-OnlyHuman.mp3',
+      title: 'Jonas Brothers',
+      artist: 'Only Human'
+    },
+    { src: 'tracks/18-HarryStyles-WatermelonSugar.mp3',
+      title: 'Harry Styles',
+      artist: 'Watermelon Sugar'
+    },
+    { src: 'tracks/19-RanveerSingh-ApnaTimeAayega.mp3',
+      title: 'Ranveer Singh, Dub Sharma',
+      artist: 'Apna Time Aayega'
+    }
+  ]
+};
